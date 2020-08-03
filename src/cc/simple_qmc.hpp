dbd8863231a38f03754880ea28828b22de232042
Victor Zhang <zxjzxj9@gmail.com>
2020-08-03 22:52:29 +0800

    Finish NaiveQMC code

--- src/cc/simple_qmc.hpp
+++ src/cc/simple_qmc.hpp
@@ -1,5 +1,52 @@
 #include <cmath>
+#include <random>
+// #include <fmt/core.h>
 
+template <typename T>
 class NaiveQMC {
 
+public:
+    NaiveQMC(T c, T alpha, T dr): c(c), alpha(alpha), dr(dr) {
+
+    }
+
+    T inline rho_ratio(T r1, T r2) {
+        return std::pow(c*r1+1, 2)/std::pow(c*r2+1, 2)*
+            std::exp(2*alpha*(r2-r1));
+    }
+
+    T inline energy_func(T r) {
+        return (-c*(r*(alpha*(alpha*r-4)+2)+2)+
+            alpha*(2-alpha*r)-2)/(2*r*(c*r+1));
+    }
+
+    pair<T, T> sample(int maxstep=10000) {
+        T rold = 0.0;
+        T rnew = 0.0;
+        T energy = energy_func(rold);
+        T etot = energy;
+        T etot_sq = std::pow(energy, 2);
+        for(int i=0; i<maxstep; i++) {
+            rnew = rold + dist(rgen);
+            auto ratio = rho_ratio(rnew, rold);
+            if(ratio > 1 || ratio > rnum(rgen)) {
+                rold = rnew;
+                energy = energy_func(rold);
+            }
+            etot += energy;
+            etot_sq += std::pow(energy, 2);
+        }
+        auto mean = etot/static_cast<T>(maxstep);
+        auto std = std::sqrt(etot_sq/static_cast<T>(maxstep)-
+            std::powd(mean, 2));
+        return std::make_pair(mean, std);
+    }
+
+private:
+    T c, alpha;
+    T dr;
+    std::random_device rd;
+    std::mt19937 rgen{rd};
+    std::uniform_real_distribution<T> dist(-dr, dr);
+    std::uniform_real_distribution<T> rnum(0, 1);
 };
