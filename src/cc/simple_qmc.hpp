7fd3af83171d3534124567e6088c9a20be5e533d
Victor Zhang <zxjzxj9@gmail.com>
2020-08-05 15:38:15 +0800

    Fix a bug in graph.py (still have a bug in simple_qmc.hpp)

--- src/cc/simple_qmc.hpp
+++ src/cc/simple_qmc.hpp
@@ -10,9 +10,11 @@ public:
     NaiveQMC(T c, T alpha, T dr): c(c), alpha(alpha), dr(dr) {
     }
 
+    // return rho1 / rho2
     T inline rho_ratio(T r1, T r2) {
         // r1 = std::abs(r1);
         // r2 = std::abs(r2);
+        // if(r1 < 1e-6) return 0;
         return std::pow(c*r1+1, 2)/std::pow(c*r2+1, 2)*
             std::exp(2*alpha*(r2-r1));
     }
@@ -24,7 +26,7 @@ public:
     }
 
     std::pair<T, T> sample(int maxstep=10000) {
-        std::uniform_real_distribution<T> dist(-dr, dr);
+        std::uniform_real_distribution<T> dist(-1.0, 1.0);
         std::uniform_real_distribution<T> rnum(0, 1);
         T rnew, rold = 1.0;
         T energy = energy_func(rold);
@@ -32,18 +34,27 @@ public:
         // exit(0);
         T etot = 0;
         T etot_sq = 0;
+        int accept = 0;
         for(int i=0; i<maxstep; i++) {
-            rnew = rold + dist(rgen);
+            dr = std::min(dr, rold);
+            rnew = rold + dr*dist(rgen);
+            fmt::print("{}\n", rold);
             auto ratio = rho_ratio(rnew, rold);
-            if(rnew > 0 && ratio > 1 || ratio > rnum(rgen)) {
+            if(ratio > 1 || ratio > rnum(rgen)) {
                 rold = rnew;
                 energy = energy_func(rold);
+                accept++;
             }
+
+            // if(static_cast<T>(accept)/(i+1) > 0.5) dr*=scale;
+            // else dr/=scale;
+
             etot += energy;
             etot_sq += std::pow(energy, 2);
         }
         auto mean = etot/static_cast<T>(maxstep);
         // auto std = etot_sq/static_cast<T>(maxstep);
+        fmt::print("Accept ration: {:.2f}, step size: {:6.4f}, last place: {:6.4f}\n", static_cast<T>(accept)/maxstep, dr, rold);
         auto std = std::sqrt(etot_sq/static_cast<T>(maxstep) - std::pow(mean, 2));
         return std::make_pair(mean, std);
     }
@@ -53,4 +64,5 @@ private:
     T dr;
     std::random_device rd;
     std::mt19937 rgen{rd()};
+    const T scale = 1.01;
 };
--- src/utils/graph.py
+++ src/utils/graph.py
@@ -6,14 +6,22 @@ from matplotlib import cm
 
 def draw_energy_contour(file_name):
     data = np.loadtxt(file_name)
-    
-    data = np.reshape(data, (-1, 11, 11))
-    c, alpha, mean, std = [data[i, ...] for i in range(4)]
-    norm = cm.colors.Normalize(vmax=-0.4, vmin=-0.5)
-    plt.contourf(mean)
+    # data: 11x11x4
+    data = np.reshape(data, (11, 11, -1))
+    c, alpha, mean, std = [data[..., i] for i in range(4)]
+    #print(mean[3])
+    #import sys; sys.exit()
+    #norm = cm.colors.Normalize(vmax=-0.4, vmin=-0.5)
+    #print(np.max(mean), np.min(mean))
+    plt.contourf(c, alpha, mean)
     plt.xlabel("c parameter")
     plt.ylabel("alpha parameter")
     plt.savefig(file_name+".png")
 
 if __name__ == "__main__":
+    x = np.linspace(-1.0, 1.0, 11, endpoint=True)
+    y = np.linspace(0.2, 1.8, 11, endpoint=True)
+    xv, yv = np.meshgrid(x, y, sparse=False, indexing='ij')
+    print(xv[3])
+    # print(yv)
     draw_energy_contour("energy_surface.txt")    
