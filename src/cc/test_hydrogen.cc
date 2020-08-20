c3c6dbdf8b44d0d408541e0c729f9ee89f53d8fc
Victor Zhang <zxjzxj9@gmail.com>
2020-08-20 13:54:43 +0800

    Fix rvalue forward bug

--- src/cc/test_hydrogen.cc
+++ src/cc/test_hydrogen.cc
@@ -11,7 +11,7 @@ Eigen::Matrix<T, 1, 3> gradient(Eigen::Matrix<T, 1, 3> p, Fn func) {
     T coeff[] = {-1, 8, -8, 1};
     T ds[] = {+2, +1, -1, -2};
 
-    auto dfunc = [&](Eigen::Matrix<T, 1, 3> dp){
+    auto dfunc = [&](Eigen::Matrix<T, 1, 3>& dp){
         T ret = 0.0;
         for(int i=0; i<4; i++) {
             ret += coeff[i]*func(p+ds[i]*dp);
@@ -32,18 +32,22 @@ Eigen::Matrix<T, 1, 3> gradient(Eigen::Matrix<T, 1, 3> p, Fn func) {
 }
 
 TEST(SimpleGradientTEST, Gradient) {
-    auto func = [](Eigen::Matrix<double, 1, 3>  p) {
+    auto func = [](Eigen::Matrix<double, 1, 3>&& p) {
         return p.array().pow(2).sum();
     };
 
-    auto dfunc = [](Eigen::Matrix<double, 1, 3> p) {
-        return 2.0*p;
+    auto dfunc = [](Eigen::Matrix<double, 1, 3>& p) {
+        auto ret = 2.0*p;
+        return ret;
     };
 
     Eigen::Matrix<double, 1, 3> p = Eigen::Matrix<double, 1, 3>::Random();
+    // std::cout<<p<<std::endl;
     auto nderv = gradient(p, func);
+    // std::cout<<nderv<<std::endl;
     auto derv = dfunc(p);
-    ASSERT_DOUBLE_EQ((nderv - derv).norm(), 0.0);
+    // std::cout<<derv<<std::endl;
+    ASSERT_NEAR((nderv - derv).norm(), 0.0, 1e-6);
 }
 
 //TEST(AtomicWaveFnTEST, Gradient) {
