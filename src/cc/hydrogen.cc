55a92265838763f91714977e1279444af3c53832
Victor Zhang <zxjzxj9@gmail.com>
2020-09-11 22:04:19 +0800

    Add some options

--- src/cc/hydrogen.cc
+++ src/cc/hydrogen.cc
@@ -1,4 +1,5 @@
 #include <fmt/core.h>
+#include <cxxopts.hpp>
 #include "hydrogen.hpp"
 
 int main() {
@@ -8,5 +9,14 @@ int main() {
     r1 << 0.40, 0.0, 0.0;
     r2 << -0.40, 0.0, 0.0;
     H2MolQMC<double> h2qmc(1.0, 0.5, 1.0, r1, r2, 0.1);
+    cxxopts::Options options("HydrogenQMC", "Quantum Monte Carlo Program for Hydrogen Molecule");
+    options.add_options()
+        ("r,range", "Use range exploration", cxxopts::value<bool>()->default_value("false"))
+        ("c", "Trial wave function parameter c", cxxopts::value<double>()->default_value("0.0"))
+        ("alpha", "Trial wave function parameter alpha", cxxopts::value<double>()->default_value("1.0"))
+        ("s,step", "Monte Carlo step size", cxxopts::value<double>()->default_value("1.0"))
+        ("n,nstep", "Monte Carlo step size", cxxopts::value<int>()->default_value("1000000"))
+        ("h,help", "Print usage")
+    ;
     return 0;
 }
