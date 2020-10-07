85d943be71dc7b04d2b383470193f8ae89f02e17
Victor Zhang <zxjzxj9@gmail.com>
2020-10-07 22:14:47 +0800

    Add basic codes for lithium. [ci skip]

--- src/cc/lithium.hpp
+++ src/cc/lithium.hpp
@@ -1,2 +1,26 @@
 #pragma once
 
+template<typename T>
+class WaveFn {
+public:
+    virtual ~WaveFn(){};
+    // return the value of wave function
+    virtual T value(const PCoord<T>&)=0;
+
+    // return three components of grad wave function
+    virtual PCoord<T> grad(const PCoord<T>&)=0;
+    
+    // return laplacian of the wave function
+    virtual T laplace(const PCoord<T>&)=0;
+};
+
+template<typename T>
+class SlaterWaveFn {
+public:
+    SlaterWaveFn(){
+
+    }
+    ~SlaterWaveFn(){};
+private:
+
+};
